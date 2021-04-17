3c85ad9ba14d95ce58acecbb7ef1fec9515b2dd0
jdev <dct@DCT>
2021-04-17 16:14:10 +0300

    generate dependency property changed fallback method? solving issue

--- ViewModels/SigningStatusViewModel.cs
+++ ViewModels/SigningStatusViewModel.cs
@@ -1,26 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using ImageBrowser.Helpers;
-using System.Threading.Tasks;
+﻿using ImageBrowser.Helpers;
+using System;
+using Windows.UI.Xaml;
 
 namespace ImageBrowser.ViewModels
 {
-    public class SigningStatusViewModel : BindableBase
+    public class SigningStatusViewModel : DependencyObject
     {
+        public static readonly DependencyObject dependencyObject;
+        public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(
+  "IsUserSignedOut",
+  typeof(Boolean),
+  typeof(MSGraphQueriesHelper),
+  new PropertyMetadata(null, new PropertyChangedCallback(OnStatusChanged))
+);
+        // TODO silving falling down issue  
+        private static void OnStatusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SigningStatusViewModel iwlc = (SigningStatusViewModel)d ; //null checks omitted
+            bool s = (bool)e.NewValue ; //null checks omitted
+            if (s == false)
+            {
+                iwlc.IsUserSignedOut = false;
+            }
+            else
+            {
+                iwlc.IsUserSignedOut = true;
+            }
+        }
 
         private bool _isUserSignedOut = MSGraphQueriesHelper.UserSignedOut;
-       public  bool IsUserSignedOut
+        public bool IsUserSignedOut
         {
-            get { return MSGraphQueriesHelper.UserSignedOut;  }
-            set { base.SetProperty(ref _isUserSignedOut, MSGraphQueriesHelper.UserSignedOut); }
+            get { /*return MSGraphQueriesHelper.UserSignedOut;*/return (bool)GetValue(StatusProperty); }
+            set
+            { /*base.SetProperty(ref _isUserSignedOut, MSGraphQueriesHelper.UserSignedOut);*/
+                SetValue(StatusProperty,MSGraphQueriesHelper.UserSignedOut);
+            }
         }
 
         public SigningStatusViewModel()
         {
+            //RegisterPropertyChangedCallback(StatusProperty, IsUserSignedOut);
             IsUserSignedOut = MSGraphQueriesHelper.UserSignedOut;
         }
+
     }
 }
