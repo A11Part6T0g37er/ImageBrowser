dda13969e4802112caca92bef6a13362f6711665
jdev <dct@DCT>
2021-04-19 15:46:11 +0300

    Add dependencyObject callback handler

--- ViewModels/SigningStatusViewModel.cs
+++ ViewModels/SigningStatusViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using ImageBrowser.Helpers;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Windows.UI.Xaml;
 
 namespace ImageBrowser.ViewModels
 {
-    public class SigningStatusViewModel : BindableBase /* DependencyObject*/
+    public class SigningStatusViewModel :/* BindableBase */ DependencyObject, INotifyPropertyChanged
     {
         public static readonly DependencyObject dependencyObject;
         public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(
@@ -18,31 +20,41 @@ namespace ImageBrowser.ViewModels
         {
             bool oldValue = (bool)e.OldValue;
             bool newValue = MSGraphQueriesHelper.UserSignedOut;
-
+            SigningStatusViewModel signingStatus = d as SigningStatusViewModel;
+            signingStatus?.OnStatusChanged();
         }
 
-        private bool _isUserSignedOut;/* = MSGraphQueriesHelper.UserSignedOut;*/
+        protected virtual void OnStatusChanged()
+        {
+            OnPropertyChanged("IsUserSignedOut");
+        }
+
+        private bool _isUserSignedOut;/* for accessor and mutator */
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public bool IsUserSignedOut
         {
             get
             {
-                // return MSGraphQueriesHelper.UserSignedOut;
-                //return (bool)GetValue(StatusProperty);
-                return _isUserSignedOut;
+                //return MSGraphQueriesHelper.UserSignedOut;
+                return (bool)GetValue(StatusProperty);
+                //return _isUserSignedOut;
 
             }
             set
             {
-                base.SetProperty(ref _isUserSignedOut, MSGraphQueriesHelper.UserSignedOut);
-                //SetValue(StatusProperty, MSGraphQueriesHelper.UserSignedOut);
+                //base.SetProperty(ref _isUserSignedOut, MSGraphQueriesHelper.UserSignedOut);
+                SetValue(StatusProperty, MSGraphQueriesHelper.UserSignedOut);
             }
         }
 
-        public SigningStatusViewModel()
+     /*   public SigningStatusViewModel()
         {
             // RegisterPropertyChangedCallback(IsUserSignedOut, OnStatusChanged);
             //  IsUserSignedOut = MSGraphQueriesHelper.UserSignedOut;
-        }
-        
+        }*/
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
