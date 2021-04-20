80a4a461dd3dbd79350628b5b81cbb8e2bc9cae4
jdev <dct@DCT>
2021-04-20 16:08:34 +0300

    Command realization

--- ViewModels/SigningStatusViewModel.cs
+++ ViewModels/SigningStatusViewModel.cs
@@ -1,8 +1,14 @@
-﻿using ImageBrowser.Helpers;
+﻿using ImageBrowser.Common;
+using ImageBrowser.Helpers;
+using Microsoft.Identity.Client;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 
 namespace ImageBrowser.ViewModels
@@ -58,6 +64,32 @@ namespace ImageBrowser.ViewModels
 
         public SigningStatusViewModel()
         {
+         /*   SignOutCommand = new RelayCommand(async()=> {
+                IEnumerable<IAccount> accounts = await MSGraphQueriesHelper.GetMSGraphAccouts();
+                if (accounts == null)
+                    return;
+                IAccount firstAccount = accounts.FirstOrDefault();
+
+                try
+                {
+                    await MSGraphQueriesHelper.SingOutMSGraphAccount(firstAccount).ConfigureAwait(false);
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        ResultText.Text = "User has signed-out";
+                        signingOneDrive.Visibility = Visibility.Visible;
+                        OpenOneDrive.Visibility = Visibility.Collapsed;
+                        OneDriveInfo.Text = "";
+                        imageFileInfoViewModel.FlushObservableCollectionOfImages();
+                    });
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            });*/
+
+
+
 
             MSGraphQueriesHelper.PropertyChanged += SigningStatusViewModel_OnStatusChanged;
         }
@@ -71,5 +103,10 @@ namespace ImageBrowser.ViewModels
 
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        ICommand SignOutCommand { get; set; }
+
+
     }
 }
