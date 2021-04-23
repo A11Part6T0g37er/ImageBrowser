75684acfc69c43a2251138e9c733b582c45fe851
jdev <dct@DCT>
2021-04-23 15:07:55 +0300

    Add helper for UI thread control

---
+++ Helpers/CallUIThreadHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace ImageBrowser.Helpers
+{
+   public static  class CallUIThreadHelper
+    {
+        /// <summary>
+        /// Runs the specified handler on the UI thread at Normal priority. 
+        /// </summary>
+        public static async Task CallOnUiThreadAsync(DispatchedHandler handler) => await
+            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, handler);
+    }
+}
