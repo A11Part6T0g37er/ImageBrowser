1964f379bd038cd05d85156c64cc1e45873d6cd1
jdev <dct@DCT>
2021-05-15 16:10:50 +0300

    Initialize file handler association.

--- App.xaml.cs
+++ App.xaml.cs
@@ -130,5 +130,21 @@ namespace ImageBrowser
                 e.Handled = TryGoBack();
             }
         }
+
+        /// <summary>
+        /// Associate with image file extensions.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            base.OnFileActivated(args);
+
+
+            var rootFrame = new Frame();
+            rootFrame.Navigate(typeof(DetailPage), args);
+            Window.Current.Content = rootFrame;
+            Window.Current.Activate();
+        }
+
     }
 }
---
+++ Views/OpenWithPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ImageBrowser.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OpenWithPage : Page
+    {
+        public OpenWithPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //----------------< OnNavigatedTo() >----------------
+            //* when opened by file-extension with arguments
+            base.OnNavigatedTo(e);
+            var args = e.Parameter as Windows.ApplicationModel.Activation.IActivatedEventArgs;
+            if (args != null)
+            {
+                if (args.Kind == Windows.ApplicationModel.Activation.ActivationKind.File)
+                {
+                    //--< IsOpened by File >--
+                    //-< get FilePath >-
+                    var fileArgs = args as Windows.ApplicationModel.Activation.FileActivatedEventArgs;
+                    string strFilePath = fileArgs.Files[0].Path;
+                    //-< get FilePath >-
+
+                    //< show >
+                    TargetName.Text = strFilePath;
+                    //</ show >
+                    //--</ IsOpened by File >
+                }
+            }
+            //----------------</ OnNavigatedTo() >----------------
+        }
+    }
+}
